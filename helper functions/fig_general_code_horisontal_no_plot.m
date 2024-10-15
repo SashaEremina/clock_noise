601e0105471d9b050e5d9103589d190f4dc1e3a3
SashaEremina <39064060+SashaEremina@users.noreply.github.com>
2024-10-15 20:07:23 +0100

    Add files via upload

---
+++ helper functions/fig_general_code_horisontal_no_plot.m
@@ -0,0 +1,34 @@
+cd('C:\Users\sasha.eremina\Documents\MATLAB\clock_robust_fig');
+
+%cd([C:\Users\sasha.eremina\Documents\MATLAB\Thesis_fig\Chapt4]);
+
+hfig = figure(1);  % save the figure handle in a variable
+picturewidth = 20; % set this parameter and keep it forever
+hw_ratio = 0.8; % feel free to play with this ratio
+hw_ratio1 = 0.8; % feel free to play with this ratio
+
+  hw_ratio1 = 0.5;
+% 
+% if exist('l','var')
+%     if ~isempty(strfind(l.Location,'outside'))
+%         hw_ratio1 = 0.5;
+% 
+%     else
+%     hw_ratio1 = 0.5;
+%     end
+% end
+
+set(findall(hfig,'-property','FontSize'),'FontSize',16) % adjust fontsize to your document
+
+set(findall(hfig,'-property','Box'),'Box','off') % optional
+set(findall(hfig,'-property','Interpreter'),'Interpreter','latex') 
+set(findall(hfig,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
+if hw_ratio1==0.8
+    set(hfig,'Units','centimeters','Position',[3 3 picturewidth hw_ratio*picturewidth])
+else
+    set(hfig,'Units','centimeters','Position',[3 3 picturewidth*1.5 hw_ratio*picturewidth])
+end    
+pos = get(hfig,'Position');
+set(hfig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
+%print(hfig,fname,'-dpdf','-painters','-fillpage')
+%print(hfig,fname,'-dpng','-painters')
