fccd7dcdfbfeecfc275d693402c64abf76f47698
SashaEremina <39064060+SashaEremina@users.noreply.github.com>
2024-12-06 17:50:14 +0000

    Add files via upload

---
+++ helper functions/fig_save_font_16.m
@@ -0,0 +1,27 @@
+
+%Location
+if ~contains(pwd,strcat(selpath,'/figures'))
+    cd([selpath,'/figures']);
+end
+
+hfig = figure(1);  % save the figure handle in a variable
+picturewidth = 20; % set this parameter and keep it forever
+hw_ratio = 0.8; % feel free to play with this ratio
+hw_ratio1 = 0.8; % feel free to play with this ratio
+
+set(findall(hfig,'-property','FontSize'),'FontSize',16)
+
+set(findall(hfig,'-property','Box'),'Box','off') % optional
+set(findall(hfig,'-property','Interpreter'),'Interpreter','latex') 
+set(findall(hfig,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
+if hw_ratio1==0.8
+    set(hfig,'Units','centimeters','Position',[3 3 picturewidth hw_ratio*picturewidth])
+else
+    set(hfig,'Units','centimeters','Position',[3 3 picturewidth*1.3 hw_ratio*picturewidth])
+end    
+pos = get(hfig,'Position');
+set(hfig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
+print(hfig,fname,'-dpdf','-vector','-fillpage');
+print(hfig,fname,'-dpng','-vector');
+print(hfig,fname,'-dsvg','-vector');
+
